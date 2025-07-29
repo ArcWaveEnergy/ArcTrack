92d2a5b4f2115f2945971ac08caf7896f4ef1cb9
ArcWaveEnergy <adamsclint1989@gmail.com>
2025-07-29 13:58:00 -0500

    Delete gps.js

--- gps.js
+++
@@ -1,8 +0,0 @@
-function getCurrentPosition(cb){
-  if(!navigator.geolocation){ alert('Geolocation not supported'); return; }
-  navigator.geolocation.getCurrentPosition(
-    pos => cb({lat: pos.coords.latitude, lng: pos.coords.longitude, ts: new Date().toISOString()}),
-    err => alert('GPS error: ' + err.message),
-    {enableHighAccuracy:true, timeout:10000, maximumAge:30000}
-  );
-}
